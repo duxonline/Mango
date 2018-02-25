a021042ea2f120d4d374caec1ea21701942eed3b
Frank Wang <frankwangsydney@gmail.com>
2018-02-25 18:25:11 +1100

    Passed the first unit test

--- main.go
+++ main.go
@@ -5,15 +5,25 @@ import (
 	"net/http"
 
 	"github.com/frankwang0/MangoCommerce/app/catalogue"
-	_ "github.com/frankwang0/MangoCommerce/config"
 	"github.com/frankwang0/MangoCommerce/routes"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	LoadEnv()
+
 	r := mux.NewRouter()
 	r.Handle("/products", routes.RequestHandler(catalogue.CreateCategory)).Methods("POST")
 	r.Handle("/products/{id}", routes.RequestHandler(catalogue.GetCategory)).Methods("GET")
 	http.Handle("/", r)
+
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
+
+func LoadEnv() {
+	fileErr := godotenv.Load("config/.env")
+	if fileErr != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
