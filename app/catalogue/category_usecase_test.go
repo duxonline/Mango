e1f18d85687f8c82333b9e550e2bcd88518bc8fb
Frank Wang <frankwangsydney@gmail.com>
2018-02-27 09:35:11 +1100

    Addded another unit test for adding child catgegory

--- app/catalogue/category_usecase_test.go
+++ app/catalogue/category_usecase_test.go
@@ -3,6 +3,8 @@ package catalogue
 import (
 	"testing"
 
+	"github.com/frankwang0/MangoCommerce/common"
+
 	"github.com/joho/godotenv"
 )
 
@@ -10,7 +12,7 @@ func init() {
 	godotenv.Load("../../config/.env")
 }
 
-func Test_store_admins_add_top_category(t *testing.T) {
+func Test_store_admins_add_parent_category(t *testing.T) {
 	// Arrange
 	var name = "Books"
 	request := CreateCategoryRequest{
@@ -32,6 +34,44 @@ func Test_store_admins_add_top_category(t *testing.T) {
 	}
 
 	if category.ParentID != 0 {
+		t.Errorf("Category parentId is not saved correctly.")
+	}
+}
+
+func Test_store_admins_add_child_category(t *testing.T) {
+	// Arrange
+	var parentName = "Books"
+	parentRequest := CreateCategoryRequest{
+		Name:     parentName,
+		ParentID: 0,
+	}
+
+	sut := CreateUseCase()
+	parentCategory, _ := sut.CreateCategory(parentRequest)
+
+	var categoryName = "Programming Books"
+	childRequest := CreateCategoryRequest{
+		Name:     categoryName,
+		ParentID: parentCategory.ID,
+	}
+
+	// Act
+	childCategory, createErr := sut.CreateCategory(childRequest)
+
+	// Assert
+	AssertCategory(t, childCategory, createErr, categoryName, parentCategory.ID)
+}
+
+func AssertCategory(t *testing.T, category *Category, createErr *common.AppError, name string, parentID int) {
+	if createErr != nil {
+		t.Errorf("Creating category throws an error.")
+	}
+
+	if category.Name != name {
+		t.Errorf("Category name is not saved correctly.")
+	}
+
+	if category.ParentID != parentID {
 		t.Errorf("Category parentId is not saved correctly.")
 	}
 }
